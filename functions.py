26531074ccb33fbc6eb806aa1f015bdb2bfbc900
Desmond Mpofu <desmondm@comunityplatform.com>
2025-04-20 17:30:16 +0200

    tasks and routes

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import mathsRunctions
 
-for i in range(5):
-    print(f"The square of {i} is {mathsRunctions.squares(i)}")
-    print(f"The cube of {i} is {mathsRunctions.cubes(i)}")
-    print(f"The square root of {i} is {mathsRunctions.square_root(i)}")
+for i in range(10):
+    # print(f"The square of {i} is {mathsRunctions.squares(i)}")
+    # print(f"The cube of {i} is {mathsRunctions.cubes(i)}")
+    # print(f"The square root of {i} is {mathsRunctions.square_root(i)}")
+    print(f' Multiplied  7 by {i} is {mathsRunctions.multiply_by(i)}')
--- mathsRunctions.py
+++ mathsRunctions.py
@@ -1,8 +1,11 @@
-def squares(n):
-    return n * n
+# def squares(n):
+#     return n * n
     
-def cubes(n):
-    return n * n * n
+# def cubes(n):
+#     return n * n * n
 
-def square_root(n):
-    return n ** 0.5
+# def square_root(n):
+#     return n ** 0.5
+
+def multiply_by (n):
+    return n*7
