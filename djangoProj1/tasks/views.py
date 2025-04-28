99d86205e820e8b912f66ab2c1230edb06b95d76
Desmond Mpofu <desmondm@comunityplatform.com>
2025-04-28 08:04:34 +0200

    add task

--- djangoProj1/tasks/views.py
+++ djangoProj1/tasks/views.py
@@ -1,28 +1,32 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 
 class NewTaskForm(forms.Form):
     taskTitle =forms.CharField(label="New Task title")
     taskDescription =forms.CharField(label="Task description")
+    
 
 tasks = [
     {
-     'id': 1,
+     'id': '1',
      'title': 'Bath',
     'description': 'Take a bath',
     },
     {
-     'id': 2,
+     'id': '2',
      'title': 'Dress',
     'description': 'Get dressed',
     },
     {
-     'id': 3,
+      'id': '3',
      'title': 'Breakfast',
     'description': 'Eat breakfast',
     },
     {
-     'id': 4,
+     'id': '4',
      'title': 'Work',
     'description': 'Work on the project',
     }
@@ -34,6 +38,20 @@ def index(request):
     })
 
 def add(request):
+    if request.method=="POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task =  {
+                    'title': form.cleaned_data["taskTitle"],
+                    'description': form.cleaned_data["taskDescription"]
+                    }
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html",{
+                'form':form
+            })
+
     return render(request, 'tasks/add.html', {
         'form': NewTaskForm()
     })
