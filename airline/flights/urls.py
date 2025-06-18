60be046ec0458421fdc6666eddfe8ea3dcd906d3
Desmond Mpofu <desmondm@comunityplatform.com>
2025-06-18 18:18:29 +0200

    remove booking

--- airline/flights/urls.py
+++ airline/flights/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
    path('', views.index, name='index'),
    path ('<int:flight_id>', views.flightFunc, name='flight'),
-   path('<int:flight_id>/book', views.book, name='book' )
+   path('<int:flight_id>/book', views.book, name='book' ),
+   path('<int:flight_id>/<int:passenger_id>', views.cancelBooking, name='cancelBooking')
 ]
--- airline/flights/views.py
+++ airline/flights/views.py
@@ -29,3 +29,15 @@ def book(request, flight_id):
         passenger=Passenger.objects.get(pk= int(request.POST['passenger']))
         passenger.flights.add(flight)
         return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
+
+def cancelBooking(request, flight_id, passenger_id):
+    if request.method == 'POST':  # Good practice to require POST for actions that modify data
+        try:
+            passenger = Passenger.objects.get(pk=passenger_id)  # Fixed: use objects.get instead of just get
+            flight = Flight.objects.get(pk=flight_id)
+            passenger.flights.remove(flight)
+            return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
+        except Passenger.DoesNotExist:
+            # Handle case where passenger doesn't exist
+            return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
+       
